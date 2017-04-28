ed4c815ccb4babd6bda67ade2a943bf06a8ff3e5
jinglf000 <jinglf000@gmail.com>
2017-04-28 18:10:21 +0800

    state状态

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * 测试服务
+ * @author jinglf000
+ */
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.set('port',process.env.PORT || 8080);
+
+app.use(express.static(path.join(__dirname,"/src")));
+app.use(express.static(path.join(__dirname,"/node_modules")));
+
+app.listen(app.get('port'));
+
+console.log("server is running at 8080");
